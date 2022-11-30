900bc8a7094beeb519c86a7b1d07a9986524dacf
Samu_l <69546753+tetr-git@users.noreply.github.com>
2022-11-30 14:48:30 +0100

    Added Jump

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,6 +8,14 @@ public class PlayerScript : MonoBehaviour
     public float speed = 5.0f;
     public float rotationSpeed = 0.2f;
     public float playerAngle = 0;
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+    
+    Rigidbody _rb;
+    void Start(){
+        _rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
     void FixedUpdate()
     {
         float moveForward = Input.GetAxis("Vertical") * speed * Time.deltaTime;
@@ -25,5 +33,9 @@ public class PlayerScript : MonoBehaviour
         }
         Vector3 targetDirection = new Vector3(Mathf.Sin(playerAngle), 0, Mathf.Cos(playerAngle));
         transform.rotation = Quaternion.LookRotation(targetDirection);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+        }
     }
 }
