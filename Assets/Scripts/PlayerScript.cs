291278d3a2627f3963d3891b9e2f384cc41fa1c5
Samu_l <69546753+tetr-git@users.noreply.github.com>
2022-12-14 14:26:11 +0100

    Added spawn

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -11,16 +11,19 @@ public class PlayerScript : MonoBehaviour
     public float playerAngle = 0;
     public Vector3 jump;
     public float jumpForce = 2.0f;
-
     public String vertical = "VerticalPlayerOne";
     public String horizontal = "HorizontalPlayerOne";
-    public KeyCode viewMin = KeyCode.Q;
-    public KeyCode viewPlus = KeyCode.E;
-    
-    Rigidbody _rb;
+    public KeyCode viewMinKey = KeyCode.Q;
+    public KeyCode viewPlusKey = KeyCode.E;
+    public KeyCode jumpKey = KeyCode.LeftShift;
+    public Vector3 startPosition = new Vector3(26.0f, 10.0f, 26.0f);
+    private Rigidbody _rb;
+    private Vector3 _respawnPosition;
+   
     void Start(){
         _rb = GetComponent<Rigidbody>();
         jump = new Vector3(0.0f, 2.0f, 0.0f);
+        _respawnPosition = startPosition;
     }
     void FixedUpdate()
     {
@@ -29,19 +32,27 @@ public class PlayerScript : MonoBehaviour
         transform.Translate(0, 0, moveForward); 
         transform.Translate(moveSideways,0,0);
         
-        if (Input.GetKey(viewMin))
+        if (Input.GetKey(viewMinKey))
         { 
             playerAngle -= rotationSpeed;
         }
-        else if (Input.GetKey(viewPlus))
+        else if (Input.GetKey(viewPlusKey))
         {
             playerAngle += rotationSpeed;
         }
         Vector3 targetDirection = new Vector3(Mathf.Sin(playerAngle), 0, Mathf.Cos(playerAngle));
         transform.rotation = Quaternion.LookRotation(targetDirection);
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(jumpKey))
         {
             _rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+        }
+
+        //on release addfore jumpforce and null jumpforce
+        //on press add to jumpforce
+
+        if (transform.position.y<-2)
+        {
+            transform.position = _respawnPosition;
         }
     }
 }
